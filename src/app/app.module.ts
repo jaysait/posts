ef98861c5b33f00ea1cacb9578c7159922fcea47
Jason Fleming <jason.fleming.se@gmail.com>
2019-02-03 15:51:31 -0700

    initial up to file upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PostsComponent} from './posts/posts.component';
+import {PostCreateComponent} from './posts/post-create/post-create.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
+import {HeaderComponent} from './header/header.component';
+import {PostListComponent} from './posts/post-list/post-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, PostsComponent, PostCreateComponent, HeaderComponent, PostListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    HttpClientModule,
+    MatProgressSpinnerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
